32b91186f090cefb1f052126e91fb394dab2d59f
Anthony4dev <anthony.islan2005@gmail.com>
2024-03-06 19:12:18 -0300

    atualizando os setters e getters

--- src/modelo/dominio/usuario.java
+++ src/modelo/dominio/usuario.java
@@ -1,7 +1,7 @@
 package modelo.dominio;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class usuario {
 
@@ -9,11 +9,100 @@ public class usuario {
     private String nome;
     private String senha;
     private String usuario;
-
     private Perfil perfil;
 
     private boolean estado;
 
     private LocalDateTime data_hora_criacao;
     private LocalDateTime ultimoLogin;
+
+    public usuario(){
+
+    }
+
+    public usuario (Long id, String nome, String senha, String usuario, Perfil perfil, boolean estado, LocalDateTime data_hora_criacao, LocalDateTime ultimoLogin){
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+        this.usuario = usuario;
+        this.perfil = perfil;
+        this.estado = estado;
+        this.data_hora_criacao = data_hora_criacao;
+        this.ultimoLogin = ultimoLogin;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Perfil getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public LocalDateTime getData_hora_criacao() {
+        return data_hora_criacao;
+    }
+
+    public void setData_hora_criacao(LocalDateTime data_hora_criacao) {
+        this.data_hora_criacao = data_hora_criacao;
+    }
+
+    public LocalDateTime getUltimoLogin() {
+        return ultimoLogin;
+    }
+
+    public void setUltimoLogin(LocalDateTime ultimoLogin) {
+        this.ultimoLogin = ultimoLogin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof usuario)) return false;
+        usuario usuario = (usuario) o;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
